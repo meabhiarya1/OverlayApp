8d483c297ead1e863f2286328d4f9bcb9de3f10b
Abhishek kumar <abhi.arya97161@gmail.com>
2025-04-17 12:59:18 +0530

    Implement invisible overlay window functionality and update configuration

--- main.cpp
+++ main.cpp
@@ -2,61 +2,59 @@
 #include <dwmapi.h>
 #include <d3d11.h>
 #include <tchar.h>
-
-#define IMGUI_IMPL_WIN32_ENABLE
-
 #include "imgui.h"
 #include "backends/imgui_impl_win32.h"
 #include "backends/imgui_impl_dx11.h"
 
-
-
-// Link required libraries
 #pragma comment(lib, "dwmapi.lib")
 #pragma comment(lib, "d3d11.lib")
 
-// Globals
 HWND hwnd = nullptr;
 ID3D11Device *g_pd3dDevice = nullptr;
 ID3D11DeviceContext *g_pd3dDeviceContext = nullptr;
 IDXGISwapChain *g_pSwapChain = nullptr;
 ID3D11RenderTargetView *g_mainRenderTargetView = nullptr;
 
-// Forward declarations
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam); // 👈 Add this!
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 void CreateRenderTarget();
 void CleanupRenderTarget();
 bool CreateDeviceD3D(HWND hWnd);
 void CleanupDeviceD3D();
 
+void SetWindowInvisible(HWND hwnd)
+{
+    // Make the window invisible to the screen capture tools (e.g., Meet)
+    SetWindowLongPtr(hwnd, GWL_EXSTYLE, GetWindowLongPtr(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_TOOLWINDOW | WS_EX_NOACTIVATE);
+    SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 0, LWA_COLORKEY); // Transparent but invisible to capture tools
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 {
-    // Register class
     WNDCLASSEX wc = {
         sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L,
         GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
         _T("OverlayClass"), NULL};
     RegisterClassEx(&wc);
 
-    // Create window (borderless, topmost, transparent, layered)
     hwnd = CreateWindowEx(
-        WS_EX_TOPMOST | WS_EX_LAYERED | WS_EX_TRANSPARENT,
-        wc.lpszClassName, _T("Overlay Window"),
-        WS_POPUP, 0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
-        NULL, NULL, wc.hInstance, NULL);
+        WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_TOOLWINDOW | WS_EX_NOACTIVATE, // Transparent window
+        wc.lpszClassName,
+        _T("Invisible Overlay"),
+        WS_POPUP, // Popup style for no taskbar entry
+        0, 0,
+        GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), // Fullscreen size
+        NULL, NULL,
+        wc.hInstance, NULL);
 
-    // Make window transparent
-    SetLayeredWindowAttributes(hwnd, 0, 255, LWA_ALPHA);
+    // Set the overlay window to be invisible to screen capture tools
+    SetWindowInvisible(hwnd);
 
-    LONG style = GetWindowLong(hwnd, GWL_EXSTYLE);
-    SetWindowLong(hwnd, GWL_EXSTYLE, style | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+    ShowWindow(hwnd, SW_SHOWNOACTIVATE); // Make it visible to you, but not interactive
+    UpdateWindow(hwnd);
 
-    // Exclude from screen capture
-    BOOL exclude = TRUE;
-    DwmSetWindowAttribute(hwnd, 0x1B /* DWMWA_EXCLUDED_FROM_CAPTURE */, &exclude, sizeof(exclude));
-
-    // Initialize Direct3D
+    // Create Direct3D Device and Swap Chain
     if (!CreateDeviceD3D(hwnd))
     {
         CleanupDeviceD3D();
@@ -64,25 +62,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
         return 1;
     }
 
-    // Show window
-    ShowWindow(hwnd, SW_SHOWDEFAULT);
-    UpdateWindow(hwnd);
-
-    // Setup Dear ImGui context
+    // Setup ImGui context and bindings
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
     (void)io;
-
     ImGui::StyleColorsDark();
 
-    // Setup Platform/Renderer backends
     ImGui_ImplWin32_Init(hwnd);
     ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
 
-    // Main loop
-    MSG msg;
-    ZeroMemory(&msg, sizeof(msg));
+    MSG msg = {};
     while (msg.message != WM_QUIT)
     {
         if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
@@ -92,36 +82,33 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
             continue;
         }
 
-        // Start ImGui frame
+        // Example of an ImGui UI that won't be visible to the interviewer
         ImGui_ImplDX11_NewFrame();
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
 
-        // Your UI
-        ImGui::Begin("Overlay");
-        ImGui::Text("This is a hidden overlay");
+        // This content won't be visible to the screen share
+        ImGui::Begin("Invisible Overlay");
+        ImGui::Text("This overlay is visible to you but invisible to the interviewer.");
         ImGui::End();
 
         ImGui::Render();
-        const float clear_color_with_alpha[4] = {0, 0, 0, 0};
+        const float clear_color_with_alpha[4] = {0.0f, 0.0f, 0.0f, 0.0f}; // Transparent background
         g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
         g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, clear_color_with_alpha);
         ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-        g_pSwapChain->Present(1, 0); // Vsync
+        g_pSwapChain->Present(1, 0);
     }
 
-    // Cleanup
     ImGui_ImplDX11_Shutdown();
     ImGui_ImplWin32_Shutdown();
     ImGui::DestroyContext();
     CleanupDeviceD3D();
     DestroyWindow(hwnd);
     UnregisterClass(wc.lpszClassName, wc.hInstance);
-
     return 0;
 }
 
-// Window procedure
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
@@ -136,13 +123,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
-// DirectX Init
 bool CreateDeviceD3D(HWND hWnd)
 {
     DXGI_SWAP_CHAIN_DESC sd = {};
     sd.BufferCount = 2;
-    sd.BufferDesc.Width = 0;
-    sd.BufferDesc.Height = 0;
     sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
     sd.BufferDesc.RefreshRate.Numerator = 60;
     sd.BufferDesc.RefreshRate.Denominator = 1;
@@ -151,32 +135,24 @@ bool CreateDeviceD3D(HWND hWnd)
     sd.OutputWindow = hWnd;
     sd.SampleDesc.Count = 1;
     sd.Windowed = TRUE;
-    sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD; // For newer systems
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 
     UINT createDeviceFlags = 0;
 #ifdef _DEBUG
     createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
-    const D3D_DRIVER_TYPE driverTypes[] = {
-        D3D_DRIVER_TYPE_HARDWARE,
-        D3D_DRIVER_TYPE_WARP,
-        D3D_DRIVER_TYPE_REFERENCE,
-    };
+
+    D3D_FEATURE_LEVEL featureLevel;
     const D3D_FEATURE_LEVEL featureLevels[] = {
-        D3D_FEATURE_LEVEL_11_0,
-        D3D_FEATURE_LEVEL_10_0,
-    };
+        D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0};
 
-    for (UINT driverTypeIndex = 0; driverTypeIndex < ARRAYSIZE(driverTypes); driverTypeIndex++)
-    {
-        if (D3D11CreateDeviceAndSwapChain(NULL, driverTypes[driverTypeIndex], NULL,
-                                          createDeviceFlags, featureLevels, ARRAYSIZE(featureLevels),
-                                          D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice,
-                                          NULL, &g_pd3dDeviceContext) == S_OK)
-            break;
-    }
+    HRESULT res = D3D11CreateDeviceAndSwapChain(
+        NULL, D3D_DRIVER_TYPE_HARDWARE, NULL,
+        createDeviceFlags, featureLevels, ARRAYSIZE(featureLevels),
+        D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice,
+        &featureLevel, &g_pd3dDeviceContext);
 
-    if (!g_pSwapChain)
+    if (res != S_OK)
         return false;
 
     CreateRenderTarget();
@@ -205,7 +181,7 @@ void CleanupDeviceD3D()
 
 void CreateRenderTarget()
 {
-    ID3D11Texture2D *pBackBuffer;
+    ID3D11Texture2D *pBackBuffer = nullptr;
     g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
     g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
     pBackBuffer->Release();
